315b980df4b43c273779102477d609c1f8d1bff9
Sanaya James <sanaya.j.james@gmail.com>
2026-03-07 21:45:54 -0700

    completed lab. developed get_response_body and load_json methods

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,20 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        # query endpoint
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        # convert to json data
+        response_body = self.get_response_body()
+        data = json.loads(response_body)
+        return data        
+
+url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+
+requester = GetRequester(url)
+
+people = requester.load_json()
+
+print(people)
